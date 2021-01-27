0f191696bdbaa5a601c2d3f9796f05005a0be886
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2021-01-27 17:26:23 -0500

    Add React-icons and add icon to a Mode-CTA component

--- src/atoms/ModeCTA.jsx
+++ src/atoms/ModeCTA.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { BsMoon } from 'react-icons/bs';
 
 const ModeCTA = () => {
   return (
     <button className='Mode-CTA'>
+        <BsMoon className='Mode-CTA__icon'/>
         <span>Dark Mode</span>
       </button>
   )
