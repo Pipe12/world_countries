5d9b32fb3539080d7c3b46ccd539beb509b44f44
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2021-02-03 12:27:11 -0500

    Create a Loading component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import Home from './pages/Home';
+import Loading from './atoms/Loading';
 import './App.css';
 
 const url = 'https://restcountries.eu/rest/v2/all';
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const [countries, setCountries] = useState([]);
 
   const fetchCountries = async () => {
+    setLoading(true);
     try {
       const response = await fetch(url);
       const countries = await response.json();
       setCountries(countries);
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
@@ -23,7 +27,11 @@ function App() {
 
   return (
     <div className="App">
-      <Home countries={countries} />
+      {
+        loading 
+          ? <Loading />
+          : <Home countries={countries} />
+      }
     </div>
   );
 }
