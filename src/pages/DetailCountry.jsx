114809eab2870d1d068f0b78ecbdf33b262c8fe2
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2021-02-14 17:29:54 -0500

    Fetch country info for DetailCountry component

--- src/pages/DetailCountry.jsx
+++ src/pages/DetailCountry.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
+import Loading from '../atoms/Loading';
 
 const DetailCountry = (props) => {
-  let { alpha3Code } = useParams();
+  const { alpha3Code } = useParams();
+  const url = `https://restcountries.eu/rest/v2/alpha/${alpha3Code}`;
+
+  const [loading, setLoading] = useState(true);
+  const [country, setCountry] = useState([])
+
+  const fetchCountry = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const country = await response.json();
+      setCountry(country);
+      setLoading(false);
+    } catch (error) {
+      console.error(error); 
+    }
+  }
+
+  useEffect(() => {
+    fetchCountry();
+  }, []);
 
   return (
     <>
-      <Link to='/'>
-        Regresar
-      </Link>
-      <h2>{alpha3Code}</h2>
+      {
+        loading 
+          ? <Loading />
+          : <>
+              <Link to='/'>
+                Regresar
+              </Link>
+              <h2>{country.name}</h2>
+            </>
+      }
     </>
   )
 }
