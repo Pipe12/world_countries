2b2d6debe62ac1cc6b9ead7ed5cb77fa5274cf7d
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2021-02-22 15:56:32 -0500

    Do any page resolve all its resources itself

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../molecules/Header';
 import Main from '../molecules/Main';
+import { Regions } from '../utils/regions';
+import Loading from '../atoms/Loading';
 
-const Home = ({ countries, regions }) => {
+const Home = () => {
+
+  const url = 'https://restcountries.eu/rest/v2/all';
+
+  const [regions, setRegions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [countries, setCountries] = useState([]);
+
+  const fetchCountries = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const countries = await response.json();
+      setCountries(countries.slice(0, 10));
+      setLoading(false);
+    } catch (error) {
+      console.error(error); 
+    }
+  }
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  useEffect(() => {
+    setRegions(Regions)
+  }, []);
+
   return (
     <>
-      <Header />
-      <Main
-        countries={countries}
-        regions={regions}
-      />
+      {
+        loading 
+          ? <Loading />
+          : <>
+              <Header />
+              <Main
+                countries={countries}
+                regions={regions}
+              />
+            </>
+      }
     </>
+    
   )
 }
 
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,58 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import DetailCountry from '../pages/DetailCountry';
-import Loading from '../atoms/Loading';
-import { Regions } from '../utils/regions';
 import '../assets/styles/App.css';
 
-const url = 'https://restcountries.eu/rest/v2/all';
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [countries, setCountries] = useState([]);
-  const [regions, setRegions] = useState([]);
-
-  const fetchCountries = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(url);
-      const countries = await response.json();
-      setCountries(countries.slice(0, 10));
-      setLoading(false);
-    } catch (error) {
-      console.error(error); 
-    }
-  }
-
-  useEffect(() => {
-    fetchCountries();
-  }, []);
-
-  useEffect(() => {
-    setRegions(Regions);
-  }, []);
-
   return (
     <div className="App">
       {
-        loading 
-          ? <Loading />
-          : (
-            <Router>
-              <Switch>
-                <Route exact path='/'>
-                  <Home 
-                    countries={countries}
-                    regions={regions}
-                  />
-                </Route>
-                <Route exact path='/country/:alpha3Code'>
-                  <DetailCountry />
-                </Route>
-              </Switch>
-            </Router>
-          ) 
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/country/:alpha3Code' component={DetailCountry} />
+          </Switch>
+        </Router>
       }
     </div>
   );
