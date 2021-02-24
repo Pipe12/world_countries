c82c6a4d75f82b181215a04db2390b8a98078492
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2021-02-24 15:15:23 -0500

    Show a border name in a button

--- src/molecules/DetailCountryBorders.jsx
+++ src/molecules/DetailCountryBorders.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import DetailCountrySubTitle from '../atoms/DetailCountrySubTitle';
 import BordersButtonGroup from './BordersButtonGroup';
 import BorderButton from '../atoms/BorderButton';
 
 const DetailCountryBorders = ({ borders }) => {
+  const [countryBorders, setCountryBorders] = useState([]);
+
+  const url = `https://restcountries.eu/rest/v2/alpha?codes=${borders.join(';')}`;
+
+  useEffect(() => {
+    const fetchCountry = async () => {
+      try {
+        const response = await fetch(url);
+        const  countryBorders = await response.json();
+        setCountryBorders(countryBorders);
+      } catch (error) {
+        console.error(error); 
+      }
+    }
+    fetchCountry();
+  }, [ url ]);
+
+  console.log(countryBorders);
+
   return (
     <div className='DetailCountryBorders'>
       <DetailCountrySubTitle text='Border Countries:' />
       <BordersButtonGroup>
         {
-          borders.map((border, index) => {
-            return <BorderButton key={index} border={border} />
+          countryBorders.map((border, index) => {
+            return <BorderButton key={index} border={border.name} />
           })
         }
       </BordersButtonGroup>
